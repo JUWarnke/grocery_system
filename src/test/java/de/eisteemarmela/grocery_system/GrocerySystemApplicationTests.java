fa0f9b01d8c248992fe434bdb05f61f1ce1dd623
Jan Warnke <49824613+Eisteemarmelade@users.noreply.github.com>
2023-02-20 23:10:17 +0100

    initializing project
    inizialized the project and added Grocery class

---
+++ src/test/java/de/eisteemarmela/grocery_system/GrocerySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package de.eisteemarmela.grocery_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrocerySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
