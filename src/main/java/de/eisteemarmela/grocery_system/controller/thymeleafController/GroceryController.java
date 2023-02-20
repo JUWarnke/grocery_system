a59c51f2dcb4fd03a7234923f7564501adf5366e
Jan Warnke <49824613+Eisteemarmelade@users.noreply.github.com>
2023-02-20 23:39:22 +0100

    set thymeleaf cache to false in properties
    set getMapping path to root path
    
    added style.css to project and set html href

--- src/main/java/de/eisteemarmela/grocery_system/controller/thymeleafController/GroceryController.java
+++ src/main/java/de/eisteemarmela/grocery_system/controller/thymeleafController/GroceryController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GroceryController {
 
-    @GetMapping("/groceries")
+    @GetMapping("/")
     public String showGroceryList( Model model ) {
         model.addAttribute( "helloworld", "Hello World!" );
         return "groceries";
