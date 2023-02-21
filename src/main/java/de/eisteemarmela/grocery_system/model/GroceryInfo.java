e60e3bc5f2083e35f9c7f2f2985110c4f650e8a4
Jan Warnke <49824613+Eisteemarmelade@users.noreply.github.com>
2023-02-21 15:02:27 +0100

    added a GroceryInfo class. should hold informations about a grocery.
    added first part of form for adding new groceries

---
+++ src/main/java/de/eisteemarmela/grocery_system/model/GroceryInfo.java
@@ -0,0 +1,12 @@
+package de.eisteemarmela.grocery_system.model;
+
+import de.eisteemarmela.grocery_system.model.entities.Store;
+
+public class GroceryInfo {
+
+    private Long id;
+    private String name;
+    private Store storeName;
+
+
+}
